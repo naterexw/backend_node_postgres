5b059bae30bf2a513174acb78bc1fbd23105f2e8
Nate Welling <nate@natewelling.com>
2020-03-29 10:26:25 +0700

    setup

---
+++ src/routes/products.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { Product } from "../db/models";
+import config from "../config";
+import fs from "fs";
+import path from "path";
+
+const router = express.Router();
+
+router.get('/images-list', (req, res) => {
+  fs.readdir(path.resolve(process.env.PWD + '/public/assets/products/'), (err, files) => {
+    files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item)).map(f => config.apiUrl + '/assets/products/' + f);
+    res.json(files);
+  });
+});
+
+router.get('/', (req, res) => {
+    Product.findAll().then(products => {
+        res.json(products);
+    })
+
+});
+
+router.get('/:id', (req, res) => {
+    Product.findByPk(req.params.id).then(product => {
+        res.json(product);
+    })
+
+});
+
+router.put('/:id', (req, res) => {
+    Product.update(req.body, {where: {id: req.params.id}}).then(([product]) => {
+        res.json(product);
+    })
+
+});
+
+router.post('/', (req, res) => {
+    let product = req.body;
+    if (product.id) {
+        delete product.id;
+    }
+    Product.create(req.body).then(product => {
+        res.json(product);
+    })
+
+});
+
+router.delete('/:id', (req, res) => {
+    Product.destroy({where: {id: req.params.id}}).then(response => {
+        res.json(response);
+    })
+
+});
+
+module.exports = router;
